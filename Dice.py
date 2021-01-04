e972aa49cae500599539fda05e9d4c83870a5b9b
zhouchun <zhouchun@kuaishou.com>
2021-01-04 16:30:26 +0800

    DiceRobot v1.4

--- Dice.py
+++ Dice.py
@@ -3,7 +3,24 @@ import json
 import random
 
 token = '76332e8b65c176da5e469809c1e21e9b9a37cafd9c195778dd126e855786c686'
+tokenFile = 'token.txt'
 userName = ''
+userNameFile = 'userName.txt'
+
+def setUp():
+    global token
+    fo = open(tokenFile, "a+")
+    fo.seek(0, 0)
+    fileContent = fo.read()
+    if len(fileContent):
+        token = fileContent
+    fo.close()
+
+    global userName
+    fo = open(userNameFile, "a+")
+    fo.seek(0, 0)
+    userName = fo.read()
+    fo.close()
 
 def postMessage(msg):
     url = 'https://oapi.dingtalk.com/robot/send?access_token=' + token
@@ -39,33 +56,55 @@ def help():
         例如: 1D20
 
     尝试控制台指令请参考:
-        -h --help
-        -t --token [token]
-        -n --name [name]
-        -q --quit
+        -h : help
+        -t : token  [token]
+        -n : name   [name]
+        -q : quit
+        -r : reset config
 
     尝试提需求请联系火鸡好哥哥 
         we-chat: zczl1994
     ''')
+
+def setUserName(newUserName):
+    print('设置用户名: ' + newUserName)
+
+    global userName
+    userName = newUserName
+    fo = open(userNameFile, "w")
+    fo.write(userName)
+    fo.close()
+
+def setToken(newToken):
+    print('设置token: ' + newToken)
+
+    global token
+    token = newToken
+    fo = open(tokenFile, "w")
+    fo.write(token)
+    fo.close()
+
+def reset():
+    os.remove(tokenFile)
+    os.remove(userNameFile)
 
 def control(inputStr):
     item = inputStr[1]
     if item == 'h':
         help()
     elif item == 't':
-        global token 
-        token = inputStr[3:]
+        setToken(inputStr[3:])
     elif item == 'n':
-        global userName 
-        userName = inputStr[3:]
+        setUserName(inputStr[3:])
+    elif item == 'r':
+        reset()
     elif item == 'q':
         quit()
 
 lastRoll = 'D20'
 def roll():
     inputStr = input('roll: ')
-
-    if len(inputStr) >= 2 and inputStr[0] == '-' and inputStr[2] == ' ':
+    if len(inputStr) >= 2 and inputStr[0] == '-':
         control(inputStr)
     elif len(inputStr) == 0 or 'D' in inputStr: 
         global lastRoll
@@ -86,6 +125,6 @@ def roll():
     else:
         help()
 
-
+setUp()
 while(1):
     roll()
